ff4ea76b25bcd1f17497cd2afc74a7078c14ee5a
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-06-02 19:13:04 -0300

    Add files via upload

---
+++ js/revell.js
@@ -0,0 +1,27 @@
+ScrollReveal({ reset: true });
+
+/*ScrollReveal().reveal('#session-princiapl', 
+    {    
+        Delay: 500, reset: false, distance: '100px' , origin: 'left',
+    }
+);*/
+ScrollReveal().reveal('#arrow', 
+    {    
+        duration: 1000, reset: false,
+    }
+);
+ScrollReveal().reveal('#card', 
+    {    
+        duration: 1000, reset: false, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#card01', 
+    {    
+        duration: 1500, reset: false, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#card02', 
+    {    
+        duration: 2000, reset: false, distance: '100px' , origin: 'left',
+    }
+);
