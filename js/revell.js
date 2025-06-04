6233875e4bdc04bcddf036ea47a8066dde8162e2
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-06-04 00:16:21 -0300

    Add files via upload

--- js/revell.js
+++ js/revell.js
@@ -1,9 +1,4 @@
 ScrollReveal({ reset: true });
-ScrollReveal().reveal('#arrow', 
-    {    
-        delay: 1000, reset: false,
-    }
-);
 ScrollReveal().reveal('#card', 
     {    
         duration: 1000, reset: false, distance: '100px' , origin: 'left',
@@ -17,5 +12,30 @@ ScrollReveal().reveal('#card01',
 ScrollReveal().reveal('#card02', 
     {    
         duration: 2000, reset: false, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#servico', 
+    {    
+        duration: 2000, reset: false, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#servico02', 
+    {    
+        duration: 2500, reset: false, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#servico03', 
+    {    
+        duration: 3000, reset: false, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('.painel', 
+    {    
+        duration: 1500, reset: false, distance: '100px' , origin: 'right',
+    }
+);
+ScrollReveal().reveal('.painel2', 
+    {    
+        duration: 1500, reset: false, distance: '100px' , origin: 'left',
     }
 );
